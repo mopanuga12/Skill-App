427f965f53ee4a6ed13e28e5c0ff087f126bc6ef
MichaelOpanuga <opanugamichael@gmail.com>
2026-09-22 15:50:18 +0100

    Skill App First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { json } from 'stream/consumers';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  function CurrentYear(){
+    let year:number = new Date().getFullYear();
+    return year;
+  }
+
+  let yr:number = CurrentYear();
+  let skills:object = fetch('https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=6d2fb34f&app_key=ce174c0029895c00f8f681b107b09bbc')
+    .then((value) => value.json())
+    .then((data) => {for (let index = 0; index < data.length; index++) {
+      const element = data[index];
+      console.log(element);      
+    }
+    })
+    .catch((error) => console.log(error));
+  
+  console.log(skills)
+  return(
+    <>
+    <h1>Skill Finder</h1>
+    <h2>We are in {yr}</h2>
+    <br></br>
+    <p></p>
+    </>
+  )
 }
 
 export default App;
